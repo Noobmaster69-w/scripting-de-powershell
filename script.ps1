e80ab34e567601262a96644feed1486a7c7e6e84
Bryan_david <brmerinoji@uide.edu.ec>
2025-12-09 03:40:45 +0000

    script

--- script.ps1
+++ script.ps1
@@ -1,4 +1,4 @@
-function Start-ProgressBar {
+function Start-ProgressBar { #Crea una función llamada Start-ProgressBar
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $true)]
@@ -6,14 +6,14 @@ function Start-ProgressBar {
         
         [Parameter(Mandatory = $true)]
         [int]$Timer
-    )
+    ) #Aqui se reciben los parametros: Title: texto que mostrará el progreso y Timer: cantidad de segundos que durará
     
-    for ($i = 1; $i -le $Timer; $i++) {
-        Start-Sleep -Seconds 1
-        $percentComplete = ($i / $Timer) * 100
-        Write-Progress -Activity $Title -Status "$i seconds elapsed" -PercentComplete $percentComplete
+    for ($i = 1; $i -le $Timer; $i++) { #Hace un loop desde 1 hasta el número de segundos indicado 
+        Start-Sleep -Seconds 1 #Espera 1 segundo en cada iteración y simula el tiempo pasando
+        $percentComplete = ($i / $Timer) * 100 #Calcula qué porcentaje ha pasado
+        Write-Progress -Activity $Title -Status "$i seconds elapsed" -PercentComplete $percentComplete #Muestra la barra en la terminal
     }
 } 
 
 # Call the function
-Start-ProgressBar -Title "Test timeout" -Timer 30
+Start-ProgressBar -Title "Test timeout" -Timer 30 #Ejecuta la función mostrando una barra de progreso durante 30 segundos.
